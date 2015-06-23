353a992b93c8098d53480420ae10bdc40c17553a
Sajjad Ashraf <ssajjadashraf@yahoo.com>
2015-06-23 16:54:20 +0500

    Lesson 02 Completed.

--- app/src/main/java/com/example/sajjad/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sajjad/sunshine/MainActivity.java
@@ -1,21 +1,10 @@
 package com.example.sajjad.sunshine;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -35,6 +24,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.forecastfragment, menu);
         return true;
     }
 
@@ -53,38 +43,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        ArrayAdapter<String> mForecastAdapter = null;
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            String[] forecastArray = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Foggy - 70/40",
-                    "Weds - Cloudy - 72/63",
-                    "Thurs - Heavy Rain - 65/56",
-                    "Sat - HELP TRAPPED IN WEATHERSTATION 60/51 ",
-                    "Sun -  Sunny - 80/68"
-            };
-
-            List<String> weakForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-            mForecastAdapter= new ArrayAdapter<String>(
-                    getActivity(),
-                    R.id.listview_forecast,
-                    R.id.list_item_forecast_textview,
-                    weakForecast
-            );
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(mForecastAdapter);
-            return rootView;
-        }
-    }
 }
